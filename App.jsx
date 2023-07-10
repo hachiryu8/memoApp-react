a95eca2ee7e3ca82af7817ebd70409c814cb6820
hachiryu8 <68916722+hachiryu8@users.noreply.github.com>
2023-07-10 15:43:41 +0900

    Create LoginScreen and SignUpScreen.

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 
-import MemoCreateScreen from "./src/screens/MemoCreateScreen";
+// import LoginScreen from "./src/screens/LoginScreen";
+import SignUpScreen from "./src/screens/SignUpScreen";
+// import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 // import MemoEditScreen from "./src/screens/MemoEditScreen";
 // import MemoListScreen from "./src/screens/MemoListScreen";
 // import MemoListDetailScreen from "./src/screens/MemoDetailScreen";
 
 export default function App() {
-  return <MemoCreateScreen />;
+  return <SignUpScreen />;
 }
