ed7bcc270f0c07e02e7a77b77fd6f49a53466c09
hachiryu8 <68916722+hachiryu8@users.noreply.github.com>
2023-07-10 16:05:53 +0900

    Make components touchable.

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 
 // import LoginScreen from "./src/screens/LoginScreen";
-import SignUpScreen from "./src/screens/SignUpScreen";
+// import SignUpScreen from "./src/screens/SignUpScreen";
 // import MemoCreateScreen from "./src/screens/MemoCreateScreen";
 // import MemoEditScreen from "./src/screens/MemoEditScreen";
-// import MemoListScreen from "./src/screens/MemoListScreen";
+import MemoListScreen from "./src/screens/MemoListScreen";
 // import MemoListDetailScreen from "./src/screens/MemoDetailScreen";
 
 export default function App() {
-  return <SignUpScreen />;
+  return <MemoListScreen />;
 }
